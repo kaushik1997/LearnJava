d753b72c048702b01d4b57ef52c7c4ed53e9854a
kaushik sathvara <kaushiksathvara74@gmail.com>
2018-09-11 20:13:50 +0530

    Update DrawTickTacToeBoard.java

--- Lesson-01/Activities/ChallengeActivity2-DrawTickTacToeBoard/src/ticktactoeboard/DrawTickTacToeBoard.java
+++ Lesson-01/Activities/ChallengeActivity2-DrawTickTacToeBoard/src/ticktactoeboard/DrawTickTacToeBoard.java
@@ -15,6 +15,16 @@ public class DrawTickTacToeBoard {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("     |     |     ");
+		System.out.println("  X  |  X  |  O  ");
+		System.out.println("-----+-----+-----");
+		System.out.println("     |     |     ");
+		System.out.println("  O  |  O  |     ");
+		System.out.println("     |     |     ");
+		System.out.println("-----+-----+-----");
+		System.out.println("     |     |     ");
+		System.out.println("  X  |     |     ");
+		System.out.println("     |     |     ");
         
     }
 }
